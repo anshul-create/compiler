c08f2b4d6b274b145c25ba27bc41ed47cb394a3b
anshul-create <as9572298@gmail.com>
2026-04-23 16:44:10 +0530

    Connected both files

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import { tokenizer } from "./tokenizer.js";
 import { parser } from "./parser.js";
 
-const code = "(3 + 5) * 2";
+const code = "(3+5)*2";
 
 try {
   const tokens = tokenizer(code);
-  console.log("Tokens generated", tokens);
+  console.log('tokens');
+  console.log(tokens);
 
   const ast = parser(tokens);
 
+  console.log("\nast :");
   console.log(JSON.stringify(ast, null, 2));
 } catch (error) {
   console.log("Parsing Error", error.message);
